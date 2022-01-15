13d340135e64423a14f159a0d93dde3c27216b6d
Joelkenn1 <65873072+Joelkenn1@users.noreply.github.com>
2022-01-14 22:02:27 -0600

    Add files via upload

---
+++ PF.js
@@ -0,0 +1,12 @@
+function dochange(){
+    alert("Thank you for visiting my page");
+    
+}
+
+function intrestcolor() {
+    var d1 = document.getElementById("intrest1");
+    var d2 = document.getElementById("intrest2");
+    d1.className = "orangebackground";
+    d2.className = "bluebackground";
+
+}
